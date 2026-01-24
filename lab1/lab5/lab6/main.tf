de53f845086348d23c6e67612e1432874a1b88a8
arun7806 <arunkutty12357@.com>
2026-01-24 11:11:21 +0530

    ll

--- lab1/lab5/lab6/main.tf
+++ lab1/lab5/lab6/main.tf
@@ -53,5 +53,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+resource "azurerm_public_ip" "pip" {
+  name                = "arun-vm-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
 
 
