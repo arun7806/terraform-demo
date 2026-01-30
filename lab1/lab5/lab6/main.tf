688104876ecbfb32fa3dca2254bfef778821311f
arun7806 <arunkutty12357@.com>
2026-01-30 10:35:20 +0530

    yes

--- lab1/lab5/lab6/main.tf
+++ lab1/lab5/lab6/main.tf
@@ -1,7 +1,8 @@
 resource "azurerm_resource_group" "rg" {
   name     = "arun-rg"
-  location = "central india"
+  location = "Central India"
 }
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "arun-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -16,6 +17,14 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_public_ip" "pip" {
+  name                = "arun-vm-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "vm-nic"
   location            = azurerm_resource_group.rg.location
@@ -25,8 +34,10 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
+
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "arun-linux-vm"
   resource_group_name = azurerm_resource_group.rg.name
@@ -53,12 +64,25 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
-resource "azurerm_public_ip" "pip" {
-  name                = "arun-vm-pip"
+resource "azurerm_network_security_group" "ssh_nsg" {
+  name                = "arun-ssh-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
 
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.ssh_nsg.id
+}
 
